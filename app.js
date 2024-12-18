64ee9ec5f3163bd8a4046faab0b03ea60fe3efe6
kimlong <kimlong5244@gmail.com>
2024-12-18 10:12:11 +0700

    add khmer lang

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express from "express";
 import TelegramBot from "node-telegram-bot-api";
 import { writeFile } from "fs/promises";
 import fetch from "node-fetch"; // Required to download audio files
+import * as franc from "franc-min";
 import googleTTS from "google-tts-api";
 import dotenv from "dotenv";
-
 // Load environment variables from .env file
 dotenv.config();
 
@@ -18,20 +18,34 @@ const PORT = 3000;
 // Serve the audio files via HTTP
 app.use(express.static("audio"));
 
-// Helper function to generate text-to-speech audio
 const generateVoice = async (text, filePath) => {
+  // Detect the language of the input text
+  const detectedLang = franc.franc(text); // Returns the ISO 639-3 language code
+  let langCode = "en"; // Default language is English
+
+  console.log("Detected language code:", detectedLang);
+  // If the detected language is Khmer (ISO 639-3 code for Khmer is 'und')
+  if (detectedLang === "und") {
+    langCode = "km"; // Set language code to Khmer
+  }
+
   // Generate the Google TTS URL
   const url = googleTTS.getAudioUrl(text, {
-    lang: "en", // Language code (change to "km" for Khmer)
+    lang: langCode, // Use dynamic language code
     slow: false,
   });
 
-  // Download the audio file from the generated URL
-  const response = await fetch(url);
-  const buffer = await response.arrayBuffer();
+  try {
+    // Download the audio file from the generated URL
+    const response = await fetch(url);
+    const buffer = await response.arrayBuffer();
 
-  // Write the audio data to a file
-  await writeFile(filePath, Buffer.from(buffer));
+    // Write the audio data to a file
+    await writeFile(filePath, Buffer.from(buffer));
+  } catch (error) {
+    console.error("Error generating voice:", error);
+    throw error;
+  }
 };
 
 // Bot logic to handle user messages
@@ -41,7 +55,7 @@ bot.on("message", async (msg) => {
   if (msg?.text == "/start") {
     bot.sendMessage(
       chatId,
-      "Please send me some text, and I will convert it to speech."
+      "Please send me some text(Khmer or English), and I will convert it to speech."
     );
 
     return;
@@ -49,8 +63,7 @@ bot.on("message", async (msg) => {
 
   if (msg?.text) {
     const text = msg.text;
-    // const audioFileName = `audio_${Date.now()}.mp3`;
-    // const audioFilePath = `audio/${audioFileName}`;
+
     const audioFileName = `Preview.mp3`;
     const audioFilePath = `audio/${audioFileName}`;
 
@@ -67,7 +80,7 @@ bot.on("message", async (msg) => {
   } else {
     bot.sendMessage(
       chatId,
-      "Please send me some text, and I will convert it to speech."
+      "Please send me some text(Khmer or English), and I will convert it to speech."
     );
   }
 });
